53d238a586d4af12e937d188667ca3a07c97a018
Matteo Fava <matteo.luca.fava@gmail.com>
2024-10-18 09:48:43 +0200

    v_luce

--- lofm/v_luce/calc_all.m
+++ lofm/v_luce/calc_all.m
@@ -1,22 +1,22 @@
-function [c, sigma_c] = calc_all(file_name, f2, D, a, w_0, sigma_f2, sigma_a, sigma_D)
+function [c, sigma_c] = calc_all(file_name, f2, D, a, sigma_f2, sigma_a, sigma_D)
 %CALCOLA LA VELOCITA C MEDIA CON ERRORE ASSOCIATO CALCOLATO COME SOMMA IN
 %QUADRATURA DI MEDIA DI ERRORI SISTEMATICI E DEV STANDARD MEDIA
-% file_name = [w, d]
+% file_name = [delta_w, delta_d]
     % Carica i dati dal file (presumibilmente il file contiene w e d)
     data = load(file_name);
     
     % Supponiamo che il file contenga due colonne: w e delta_d
-    w = data(:, 1);  % prima colonna: valori di w
-    delta_d = data(:, 2);  % seconda colonna: valori di delta_d
+    delta_w = data(:, 1);  % prima colonna: valori di w
+    delta_d = data(:, 2)./1000;  % seconda colonna: valori di delta_d
     
     % Array per i valori di c e sigma_c per ciascun set di w e delta_d
-    num_data = length(w);
-    c_vals = zeros(num_data, 1);
-    sigma_c_vals = zeros(num_data, 1);
+    N = length(delta_w);
+    c_vals = zeros(N, 1);
+    sigma_c_vals = zeros(N, 1);
     
     % Loop attraverso i dati e calcolo delle singole velocità e incertezze
-    for i = 1:num_data
-        [c_vals(i), sigma_c_vals(i)] = calc_c(f2, D, w(i), w_0, a, delta_d(i), sigma_D, sigma_a, sigma_f2);
+    for i = 1:N
+        [c_vals(i), sigma_c_vals(i)] = calc_c(f2, D, delta_w(i), a, delta_d(i), sigma_D, sigma_a, sigma_f2);
     end
     
     % Calcola la media delle velocità c
@@ -25,10 +25,11 @@ function [c, sigma_c] = calc_all(file_name, f2, D, a, w_0, sigma_f2, sigma_a, si
     % Errore associato: somma in quadratura dell'errore sistematico (media degli errori)
     % e la deviazione standard della media
     sigma_c_sys = mean(sigma_c_vals);  % media degli errori sistematici
-    sigma_c_stat = std(c_vals) / sqrt(num_data);  % errore statistico (dev standard della media)
+    sigma_c_stat = std(c_vals) / sqrt(N);  % errore statistico (dev standard della media)
     
     % Errore totale combinato
     sigma_c = sqrt(sigma_c_sys^2 + sigma_c_stat^2);
 
-    printf("C = %f +- %f", c, sigma_c);
+    fprintf("Err sys:  %f  | Err stat:  %f\n", sigma_c_sys, sigma_c_stat);
+    fprintf("C = %f +- %f", c, sigma_c);
 end
--- lofm/v_luce/calc_c.m
+++ lofm/v_luce/calc_c.m
@@ -1,6 +1,5 @@
-function [c, sigma_c] = calc_c(f2, D, w, w_0, a, delta_d, sigma_D, sigma_a, sigma_f2)
-%RESTITUISCE IL VALORE 'c' E INCERTEZZA 'sigma_c' DAI DATI
-    delta_w = w-w_0;
+function [c, sigma_c] = calc_c(f2, D, delta_w, a, delta_d, sigma_D, sigma_a, sigma_f2)
+%RESTITUISCE IL VALORE 'c' E INCERTEZZA 'sigma_c' sistematico DAI DATI
     c = 4*f2*D^(2)*(delta_w)/(delta_d*(D+a-f2));
     dc_dD = (4*f2*delta_w/delta_d)*(D^2+2*D*(a+f2))/(D+a-f2)^2;
     dc_da =4*f2*D^2*delta_w/(delta_d*(D+a-f2)^2);
